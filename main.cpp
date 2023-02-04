51b1e8687a386233a445c5f408b109e2817981ab
jfoste81 <jfoste81@uncc.edu>
2023-02-03 21:56:00 -0500

    check comments on line 129, line 138 -> make players able to choose piece & fix spaces

--- main.cpp
+++ main.cpp
@@ -126,7 +126,7 @@ void move(string board[5][8], int spinAmount, vector<int>& coords, vector<string
             }
         }
     }
-    board[coords.at(count * 2)][coords.at(count * 2 + 1)] += users.at(count);
+    board[coords.at(count * 2)][coords.at(count * 2 + 1)] += users.at(count); //if-else chain to check see if space is empty
     board[y][x] = board[y][x].erase(board[y][x].find(users.at(count)),1);
 }
 
@@ -135,7 +135,7 @@ void displayBoard(string board[5][8]){
     //puts a space inside of empty indices
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 8; j++){
-            if(board[i][j] != "" && (board[i][j] != "a" || board[i][j] != "b" || board[i][j] != "c" || board[i][j] != "d")){
+            if(board[i][j] != "" && (board[i][j] != "a" || board[i][j] != "b" || board[i][j] != "c" || board[i][j] != "d")){ // change to doesnt contain a,b,c,d
                 board[i][j] == " ";
             }
         }
